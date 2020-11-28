c3a33029e2ec1c7ac991adf1e5c3089628114a08
QMallon <57999351+QMallon@users.noreply.github.com>
2020-11-27 20:58:40 -0500

    msbuildissue1

---
+++ ProgrammingClub/ProgrammingClub.Tests/UnitTest1.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class UnitTest1
+
+    <TestMethod()> Public Sub TestMethod1()
+        Dim number As Integer
+        number = 2
+        Assert.IsTrue((number Mod 2) = 0)
+    End Sub
+
+End Class
